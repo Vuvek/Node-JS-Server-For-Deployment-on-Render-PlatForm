32c5b467171f797179808177ca720ea32483b452
Vivek Kumar <vivek.9718470@gmail.com>
2023-10-07 16:54:49 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Simple HTTP Server
+
+const http = require('node:http')
+
+const server =  http.createServer((req,res) => {
+    res.writeHead(201,{"Content-Type" : "text/html"});
+    res.end("<h1>Hello World</h1>");
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT,() => {
+    console.log("Server is Up and Running on Port 3000");
+})
